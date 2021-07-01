174e532df17b5c8c1f20cc17fad3dd8ae296661a
VictoriaKochieva <kochievavictoria@gmail.com>
2021-07-01 19:22:02 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+let myStack = [];
+
+for (let i= 0; i<str.length; i++){
+   let eachEl = str[i];
+
+    if( bracketsConfig.includes(eachEl)){
+     myStack.push(eachEl);
+    } else {
+        if (myStack.length === 0){
+            return false
+        }
+
+
+        let top = myStack[myStack.length-1];
+
+        if (bracketsConfig[eachEl] === top) {
+            myStack.pop();
+        } else  {
+            return false
+        }
+    }
+}
+
+return myStack.length === 0
 }
