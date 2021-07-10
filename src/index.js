c3228b9979a56deab43e9760b7c1b5b289e9758c
VictoriaKochieva <kochievavictoria@gmail.com>
2021-07-10 17:46:36 +0300

    feat: add correct code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-
+// '()'  ---->   ['(', ')']
 let myStack = [];
+    // let bracketsPosition = {
+    // [')'] : bracketsConfig[0],
+    // [']'] : bracketsConfig[1],
+    // ['}'] : bracketsConfig[2],
+    // ['|'] :bracketsConfig[3],
+
+    // }
+
+    // let bracketsPosition2 = {
+    //     ['('] : bracketsConfig[0],
+    //     ['['] : bracketsConfig[1],
+    //     ['{'] : bracketsConfig[2],
+    //     ['|'] :bracketsConfig[3]
+    // }
+
 
 for (let i= 0; i<str.length; i++){
    let eachEl = str[i];
+   for (let j= 0; j < bracketsConfig.length; j++) {
 
-    if( bracketsConfig.includes(eachEl)){
+    if (eachEl == bracketsConfig[j][0]){
      myStack.push(eachEl);
     } else {
         if (myStack.length === 0){
             return false
         }
 
-
-        let top = myStack[myStack.length-1];
-
-        if (bracketsConfig[eachEl] === top) {
+        else if (eachEl == bracketsConfig[j][1]) {
             myStack.pop();
-        } else  {
+        } else {
             return false
         }
-    }
+
+    } 
+}
 }
 
 return myStack.length === 0
